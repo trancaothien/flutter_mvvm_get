24598388ae48d508acac4f9243ed15d1e4c026f1
trancaothien <thiennh278612@gmail.com>
2021-10-14 10:53:19 +0700

    done

--- lib/shared/widgets/widget_cached_network_image.dart
+++ lib/shared/widgets/widget_cached_network_image.dart
@@ -4,12 +4,13 @@ class WidgetCachedNetworkImage extends StatelessWidget {
   final String url;
   final Widget? errorWidget;
   final Widget? holderWidget;
-
+  final BoxShape? shape;
   const WidgetCachedNetworkImage({
     Key? key,
     required this.url,
     this.errorWidget,
     this.holderWidget,
+    this.shape = BoxShape.circle,
   }) : super(key: key);
 
   @override
